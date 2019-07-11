77eefcf5e2d92ea47bfea4fc02081506ed7f61a2
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-11 12:23:49 +0200

    initial commit ( no styling )

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  resources :articles
+
+  root 'pages#home'
+
+  get '/about', to: 'pages#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

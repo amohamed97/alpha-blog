21d4355af19c99114d106a973bf77ee1ff97ff54
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-12 13:33:51 +0200

    sign up support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'pages/home'
   get 'pages/about'
   resources :articles
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/about', to: 'pages#about'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # post 'users', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

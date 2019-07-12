28ce13f37550a34113db5a95dce26496157ec569
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-12 13:54:30 +0200

    edit user support

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)

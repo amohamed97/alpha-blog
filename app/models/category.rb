1d3a3a7a04b617ca46b57c56fbced14189712815
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-14 12:43:46 +0200

    category article association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, uniqueness: true, length: {minimum: 4, maximum: 24}
 end

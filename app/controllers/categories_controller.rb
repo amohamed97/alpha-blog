4bb84cfb909b54bca652c27927deacf17b4649d3
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-14 13:55:00 +0200

    category editing for admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-  before_action :require_admin, only: [:new]
+  before_action :require_admin, only: [:new, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 3)
@@ -25,6 +25,27 @@ class CategoriesController < ApplicationController
     @articles = @category.articles.paginate(page: params[:page], per_page: 4)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully edited"
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:success] = "Category was successfully deleted"
+    redirect_to categories_path
+  end
+
 
   private
   def category_params
@@ -33,7 +54,7 @@ class CategoriesController < ApplicationController
 
   def require_admin
     if !logged_in? || logged_in? && !current_user.admin?
-      flash[:danger] = "Only admin can create a new category"
+      flash[:danger] = "Only admin can do this action"
       redirect_to categories_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
   get 'results', to: 'articles#search'
 
   #Categories
-  resources :categories, except: [:destroy]
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

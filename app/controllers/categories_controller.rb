39177efa6c153f3c20b7acb70a9b1ad01229be25
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2019-07-14 02:11:35 +0200

    navbar with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
+
+  before_action :require_admin, only: [:new]
+
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -27,4 +30,12 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name)
   end
+
+  def require_admin
+    if !logged_in? || logged_in? && !current_user.admin?
+      flash[:danger] = "Only admin can create a new category"
+      redirect_to categories_path
+    end
+  end
+
 end
